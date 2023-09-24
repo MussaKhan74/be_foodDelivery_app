b6712410ef5f277304fcf63f43e773c888c617f4
MussaKhan74 <MarkeLoof@gmail.com>
2023-09-25 03:58:04 +0500

    added createVendor endpoint

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { CreateVendorInput } from "../dto";
+import { Vendor } from "../models";
+import { GeneratePassword, GenerateSalt } from "../utility";
 
 export const CreateVendor = async (
   req: Request,
@@ -17,16 +19,34 @@ export const CreateVendor = async (
     phone,
   } = <CreateVendorInput>req.body;
 
-  return res.json({
-    name,
-    address,
-    pincode,
-    foodType,
-    email,
-    password,
-    ownerName,
-    phone,
+  const existingVendor = await Vendor.findOne({ email: email });
+  console.log(existingVendor);
+
+  if (existingVendor !== null) {
+    return res.json({ message: "A Vendor already exist with this email ID" });
+  }
+
+  // generate a salt
+  const salt = await GenerateSalt();
+  // encrypt the password using the salt
+  const userPassword = await GeneratePassword(password, salt);
+
+  const createVendor = await Vendor.create({
+    name: name,
+    address: address,
+    pincode: pincode,
+    foodType: foodType,
+    email: email,
+    password: userPassword,
+    salt: salt,
+    ownerName: ownerName,
+    phone: phone,
+    rating: 0,
+    serviceAvailable: false,
+    coverImages: [],
   });
+
+  return res.json(createVendor);
 };
 
 export const GetVendors = async (
--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import "dotenv/config";
 
 import { AdminRoute, VendorRoute } from "./routes";
 
 const app = express();
+
+mongoose
+  .connect(`${process.env.MONGO_URI}`)
+  .then((result) => {
+    // console.log(result.connection.host);
+    console.log("Connected to DB");
+  })
+  .catch((err) => console.log("error", err));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
